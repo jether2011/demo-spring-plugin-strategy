7c75a66f9c94296148f2f50dfdd56456c3e50433
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2023-10-08 16:17:57 -0300

    feat: first structure

---
+++ src/main/kotlin/br/dev/jetherrodrigues/demo/DemoStrategyApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.jetherrodrigues.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoStrategyApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoStrategyApplication>(*args)
+}
---
+++ src/test/kotlin/br/dev/jetherrodrigues/demo/DemoStrategyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.jetherrodrigues.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoStrategyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
